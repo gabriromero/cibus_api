feab86c4a9b164f63194bdf27e3edc6da580d65c
GabriMac <gabri1998gabri@gmail.com>
2023-01-07 04:00:53 +0100

    added string length validations in user schema

--- schemas.py
+++ schemas.py
@@ -1,13 +1,13 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 # User schema
 
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
-    mail = fields.Str(required=True)
-    password = fields.Str(required=True, load_only=True)
-    name     = fields.Str(required=True)
-    last_name= fields.Str(required=True)
+    mail = fields.Str(required=True, validate=[validate.Length(min=4, max=50)])
+    password = fields.Str(required=True, load_only=True, validate=[validate.Length(min=6, max=50)])
+    name     = fields.Str(required=True, validate=[validate.Length(min=2, max=50)])
+    last_name= fields.Str(required=True, validate=[validate.Length(min=2, max=50)])
 
 class LoginUserSchema(Schema):
     mail = fields.Str(required=True)
